8c664e87582434467c872b4aa57ffb301743fe4f
Ian Clarkson <clarksonian@gmail.com>
2014-10-14 17:53:30 -0400

    Added consume.

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 
 #include "Trie.h"
 
-static const size_t kMaxWords = 0;
+static const size_t kMaxWords = 10000;
 extern unsigned char const kSowpodsAll[];
 
 class WordReceiver : public WordListener
@@ -35,79 +35,86 @@ public:
   {
     return t.anagrams(str, letters);
   }
+
+  static std::deque<std::vector<std::string>> Consume(Trie const& t, std::string const& str, size_t min_letters = 2)
+  {
+    return t.consume(str, min_letters);
+  }
 };
 
 int main(int argc, char const* agrv[])
 {
   // Create a Trie from a flat file word list
-  /*{
-    std::ifstream ifs("C:\\Users\\clarkson\\Dropbox\\Projects\\Word Lists\\sowpods.txt");
-    if (!ifs.good()) {
-      return EXIT_FAILURE;
+  std::ifstream ifs("C:\\Users\\clarkson\\Dropbox\\Projects\\Word Lists\\sowpods.txt");
+  if (!ifs.good()) {
+    return EXIT_FAILURE;
+  }
+
+  Trie h;
+  std::string word;
+  int words = 0;
+  while (std::getline(ifs, word)) {
+    if (kMaxWords && words >= kMaxWords) {
+      break;
     }
 
-    Trie h;
-    std::string word;
-    int words = 0;
-    while (std::getline(ifs, word)) {
-      if (kMaxWords && words >= kMaxWords) {
-        break;
-      }
+    h.insert(word);
+    ++words;
+  }
 
-      h.insert(word);
-      ++words;
-    }
-
-    std::ofstream ofs("trie.bin", std::ios::binary);
-    ofs << h;
-  }*/
+  //std::ofstream ofs("trie.bin", std::ios::binary);
+  //ofs << h;
 
   //{
-   /* Trie h;
-    std::ifstream ifs("trie.bin", std::ios::binary);
-    if (!ifs.good()) {
-      return EXIT_FAILURE;
-    }*/
+  /* Trie h;
+  std::ifstream ifs("trie.bin", std::ios::binary);
+  if (!ifs.good()) {
+  return EXIT_FAILURE;
+  }*/
 
-    // Recreate the trie from a file
-    //ifs >> h;
+  // Recreate the trie from a file
+  //ifs >> h;
 
-    // Retrieve all words in a flat list
-    //WordReceiver wr(h.words);
-    //h.get_words(wr);
+  // Retrieve all words in a flat list
+  //WordReceiver wr(h.words);
+  //h.get_words(wr);
 
-    //// Convert to a static
-    //std::ofstream ofs("static.txt");
-    //if (!ofs.good()) {
-    //  return EXIT_FAILURE;
-    //}
-    //int wrap = 8; int written = 0;
-    //while (!ifs.eof()) {
-    //  unsigned char c;
-    //  ifs.read(reinterpret_cast<char*>(&c), sizeof(c));
+  //// Convert to a static
+  //std::ofstream ofs("static.txt");
+  //if (!ofs.good()) {
+  //  return EXIT_FAILURE;
+  //}
+  //int wrap = 8; int written = 0;
+  //while (!ifs.eof()) {
+  //  unsigned char c;
+  //  ifs.read(reinterpret_cast<char*>(&c), sizeof(c));
 
-    //  std::stringstream ss;
-    //  ss << "0x" << std::setfill('0') << std::setw(2) << std::hex << static_cast<int>(c) << ",";
-    //  ofs << ss.str();
-    //  if (!(++written % wrap)) {
-    //    ofs << std::endl;
-    //  }
-    //}
-    //ofs.close();
+  //  std::stringstream ss;
+  //  ss << "0x" << std::setfill('0') << std::setw(2) << std::hex << static_cast<int>(c) << ",";
+  //  ofs << ss.str();
+  //  if (!(++written % wrap)) {
+  //    ofs << std::endl;
+  //  }
+  //}
+  //ofs.close();
 
-    // Recreate the trie from a static
-    Trie h;
-    Trie::read_static(kSowpodsAll, h);
+  // Recreate the trie from a static
+  //Trie h;
+  //Trie::read_static(kSowpodsAll, h);
 
-    // Generate a flat word list from the trie
-    //WordReceiver wr_static(r.words);
-    //r.get_words(wr_static);
+  // Generate a flat word list from the trie
+  //WordReceiver wr_static(r.words);
+  //r.get_words(wr_static);
   //}
 
   //auto results = WordFinder::StartsWith(h, "aba");
   //auto results = WordFinder::StartsWith(h, "");
 
-  auto results = WordFinder::Anagrams(h, "sandra");
+  //auto results = WordFinder::Anagrams(h, "sandra");
+
+  // When consuming, can just consume "depth-first", because ordering doesn't matter
+  // It will always return the words in alphabetical order, but that's OK because you can rearrange
+  auto results = WordFinder::Consume(h, "ianclarkson");
 
   return EXIT_SUCCESS;
 }
